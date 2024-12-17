5879d5b53541f3584103d6a6020fa1933bf3f006
Didzis Klavins <didzis.klavins@lyse.no>
2024-12-17 17:33:43 +0200

    Day 17 wip
    # Conflicts:
    #	Aoc2024/Aoc2024.fsproj
    #	Aoc2024/Program.fs

---
+++ Aoc2024/Dec17.fs
@@ -0,0 +1,37 @@
+﻿module Dec17
+
+open System.IO
+open StringUtils
+
+let day = 17
+
+//MetaUtils.getTodayInput day
+let filename = MetaUtils.todayFilename day
+
+let inputString = File.ReadAllText filename
+
+type MachineState =
+    {
+        A : int
+        B : int
+        C : int
+        PC: int //Program Counter
+        //Except for jump instructions, the instruction pointer increases by 2 after each instruction
+        //is processed (to move past the instruction's opcode and its operand)
+        M : byte array        
+    }
+    
+let extractRegValue line =
+    line |> split ":" |> Array.last |> trim |> int
+    
+let parseInput text =
+    let arr = text |> split "\r\n"
+    let a = extractRegValue arr[0] 
+    let b = extractRegValue arr[1] 
+    let c = extractRegValue arr[2]
+    let chars = arr[4] |> split "," |> Array.map byte
+    {A=a;B=b;C=c;PC=0;M=chars}
+    
+inputString |> parseInput |> printfn "%A"
+    
+    
--- Aoc2024/Program.fs
+++ Aoc2024/Program.fs
@@ -7,9 +7,9 @@ let filename = MetaUtils.todayFilename day
 
 let inputString = File.ReadAllText filename
 
-Dec14.calc1ex
-
-Dec14.calc1 inputString 103 101 100 |> (printfn "%A")
+// Dec14.calc1ex
+//
+// Dec14.calc1 inputString 103 101 100 |> (printfn "%A")
 // Dec14.calc1 "p=2,4 v=2,-3" 7 11 1
 //     |> Seq.iter ignore// (printfn "%A")
 //
