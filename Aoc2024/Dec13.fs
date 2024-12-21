0adc83968e2e7a3f8fd45ff795476afa89c4da0d
Didzis Klavins <didzis.klavins@lyse.no>
2024-12-21 17:48:10 +0200

    Days 13 and 18

--- Aoc2024/Dec13.fs
+++ Aoc2024/Dec13.fs
@@ -1,11 +1,128 @@
 ﻿module Dec13
 
+open System
 open System.IO
+open System.Text.RegularExpressions
+open StringUtils
 
 let day = 13
 
 MetaUtils.getTodayInput day
 let filename = MetaUtils.todayFilename day
 let inputString = File.ReadAllText filename
+let getCommands line =
+    Regex.Matches(line, "\d+") 
+    |> Seq.map (_.Value)
+    |> Seq.map int
+    |> Seq.toArray
 
+let calc1 (text:string) =
+    let lines = text.Split("\n", StringSplitOptions.RemoveEmptyEntries + StringSplitOptions.TrimEntries)
+    let t =
+        seq{
+            for i in 0..(lines.Length / 3 - 1) do
+               yield lines[3*i]+lines[3*i+1]+lines[3*i+2] 
+            }
+            
+    let mt =
+        t
+        |> Seq.map getCommands
+    
+    mt |> Seq.iter (printfn "%A")    
+    
+    let mi =
+        mt
+        |> Seq.map (fun arr->
+            let xa=int arr[0]
+            let ya=int arr[1]
+            let xb=int arr[2]
+            let yb=int arr[3]
+            let xp=int arr[4]
+            let yp=int arr[5]
+            let k = (xa*yp-ya*xp)/(xa*yb-ya*xb)
+            let n = (xp-xb*k)/xa
+            let calcX = xa * n + xb * k
+            let calcY = ya * n + yb * k
+            n,k,xp,calcX,yp,calcY
+            )
+    mi
+    |> Seq.iter (printfn "%A")    
 
+    let mcalc =
+        mi
+        |>Seq.map (fun (n,k,xp,calcX,yp,calcY)->
+            if n>= 0 && n<=100 && k>=0 && k<=100 && xp=calcX && yp=calcY then
+                n*3 + k
+            else
+                0
+            )
+        |> Seq.sum
+    mcalc
+    |>  (printfn "%A")
+
+let calc2 (text:string) =
+    let lines = text.Split("\n", StringSplitOptions.RemoveEmptyEntries + StringSplitOptions.TrimEntries)
+    let t =
+        seq{
+            for i in 0..(lines.Length / 3 - 1) do
+               yield lines[3*i]+lines[3*i+1]+lines[3*i+2] 
+            }
+            
+    let mt =
+        t
+        |> Seq.map getCommands
+    
+    mt |> Seq.iter (printfn "%A")    
+    
+    let mi =
+        mt
+        |> Seq.map (fun arr->
+            let xa=int64 arr[0]
+            let ya=int64 arr[1]
+            let xb=int64 arr[2]
+            let yb=int64 arr[3]
+            let xp=int64 arr[4] + 10000000000000L
+            let yp=int64 arr[5] + 10000000000000L
+            let k = (xa*yp-ya*xp)/(xa*yb-ya*xb)
+            let n = (xp-xb*k)/xa
+            let calcX = xa * n + xb * k
+            let calcY = ya * n + yb * k
+            n,k,xp,calcX,yp,calcY
+            )
+    mi
+    |> Seq.iter (printfn "%A")    
+
+    let mcalc =
+        mi
+        |>Seq.map (fun (n,k,xp,calcX,yp,calcY)->
+            if n>= 0 && k>=0 && xp=calcX && yp=calcY then
+                n*3L + k
+            else
+                0
+            )
+        |> Seq.sum
+    mcalc
+    |>  (printfn "%A")
+
+let Del1 () = calc1 inputString
+
+let ex = @"Button A: X+94, Y+34
+Button B: X+22, Y+67
+Prize: X=8400, Y=5400
+
+Button A: X+26, Y+66
+Button B: X+67, Y+21
+Prize: X=12748, Y=12176
+
+Button A: X+17, Y+86
+Button B: X+84, Y+37
+Prize: X=7870, Y=6450
+
+Button A: X+69, Y+23
+Button B: X+27, Y+71
+Prize: X=18641, Y=10279"
+
+let Del1ex () = calc1 ex
+let Del2ex () = calc2 ex
+
+let Del2 () = calc2 inputString
--- Aoc2024/Dec18.fs
+++ Aoc2024/Dec18.fs
@@ -2,7 +2,6 @@
 
 open System
 open System.IO
-open StringUtils
 
 let day = 18
 MetaUtils.getTodayInput day
@@ -10,5 +9,97 @@ let filename = MetaUtils.todayFilename day
 
 let inputString = File.ReadAllText filename
 
+let parse (text:string) =
+    let lines = text.Split("\n", StringSplitOptions.RemoveEmptyEntries + StringSplitOptions.TrimEntries)
+    lines
+    |> Seq.map(fun x->
+        let arr = x.Split ','
+        int arr[0], int arr[1]
+        )
+    
+let directions = [|(-1,0); (0,1); (1,0);(0,-1)|]
+
+[<TailCall>]
+let rec shortBruteForceSearch (world:int array2d) (isObstacle: int * int -> bool) (x:int,y:int) (current:int) (tx:int, ty:int)=
+    for d in directions do
+        let candX = x + fst d
+        let candY = y + snd d
+        if not (isObstacle (candX, candY)) then
+            if world[candX,candY] > current then
+                world[candX,candY] <- current
+                if candX = tx && candY = ty then
+                    printfn "%d" current
+                    ()
+                else
+                    shortBruteForceSearch world isObstacle (candX,candY) (current+1) (tx,ty)
+        
+let calc1 text w h num=
+    let world = Array2D.create w h Int32.MaxValue
+    let obstacles = 
+        parse text
+        |> Seq.take num
+        |> Set.ofSeq
+    let isObstacle (x,y) =
+        x < 0 || x >= w || y<0 || y >= h
+        || obstacles.Contains (x,y)
+    world[0,0]<-0
+    shortBruteForceSearch world isObstacle (0,0) 1 (70,70)
+    world[70,70]
+    
+    
 let Del1 () =
-    inputString |> printfn "%s"
+    let w = 70
+    let h = 70
+    calc1 inputString (w+1) (h+1) 1024
+    |> (printfn "%A")
+
+[<TailCall>]
+let rec quickBruteForceSearch (world:int array2d) (isObstacle: int * int -> bool) (x:int,y:int) (current:int) (tx:int, ty:int)=
+    if world[tx,ty]<Int32.MaxValue then
+        ()
+    else
+        for d in directions do
+            let candX = x + fst d
+            let candY = y + snd d
+            if not (isObstacle (candX, candY)) then
+                if world[candX,candY] > current then
+                    world[candX,candY] <- current
+                    if candX = tx && candY = ty then
+                        printfn "%d" current
+                        ()
+                    else
+                        quickBruteForceSearch world isObstacle (candX,candY) (current+1) (tx,ty)
+
+let pathIsOpen text w h num=
+    let world = Array2D.create w h Int32.MaxValue
+    let s =
+        parse text
+        |> Seq.take num
+    let obstacles =
+        s |> Set.ofSeq
+    let isObstacle (x,y) =
+        x < 0 || x >= w || y<0 || y >= h
+        || obstacles.Contains (x,y)
+    world[0,0]<-0
+    quickBruteForceSearch world isObstacle (0,0) 1 (70,70)
+    world[70,70] <  Int32.MaxValue, s |> Seq.last
+    
+[<TailCall>]
+let rec calcNextInterval text w h (i1, p1) (i2, p2) =
+    if i2-i1 = 1 then
+        i2, p2
+    else
+        let middle = (i1+i2) / 2
+        let middleIsOpen, pair = pathIsOpen text w h middle
+        let first, second = 
+            if middleIsOpen then
+                (middle, pair), (i2, p2)
+            else
+                (i1,p1), (middle, pair)
+        calcNextInterval text w h first second 
+
+let Del2 () =
+    let w = 70
+    let h = 70
+    calcNextInterval inputString (w+1) (h+1) (1024, (0,0)) (3500, (0,0))
+    |> snd |> (printfn "%A")
